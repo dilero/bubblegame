4c51b3d3531023556e469fd935ed71f7f7b837b3
dilero <c.dilerozdemir@gmail.com>
2015-04-07 16:19:21 +0300

    beam change lenght by time

--- test/core/src/main/java/com/bubble/actors/Beam.java
+++ test/core/src/main/java/com/bubble/actors/Beam.java
@@ -1,10 +1,13 @@
 package com.bubble.actors;
 
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+import com.bubble.utils.Constants;
 import com.bubble.utils.GameObjectFactory;
 
-public class Beam extends GameActor
-{
+public class Beam extends GameActor {
+
+	private float currentBeamHeight = Constants.BEAM_HEIGHT;
 
 	public Beam(World world, Shooter shooter) {
 		super(world);
@@ -12,5 +15,14 @@ public class Beam extends GameActor
 		body.setUserData(this);
 	}
 
+	@Override
+	public void act(float delta) {
+		currentBeamHeight =  (currentBeamHeight + (currentBeamHeight * 0.1f));
+		PolygonShape shape = (PolygonShape) body.getFixtureList().get(0)
+				.getShape();
+		shape.setAsBox(Constants.BEAM_WIDTH, currentBeamHeight);
+		body.createFixture(shape, Constants.BEAM_DENSITY);
+
+	}
 
 }
